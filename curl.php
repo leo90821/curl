5b424188d3fec35de7f4224561436497e3d5fd41
NY <nycode.jn@gmail.com>
2015-04-21 22:32:48 +0800

    update nycode
    update nycode

--- curl.php
+++ curl.php
@@ -1,19 +1,19 @@
 <?php
-$uri = "http://nycode.sinaapp.com/getpost.php";
-// 参数数组
-$data = array (
-        'name' => 'tanteng', 
-	'password' => 'password'
-);
- var_dump($data);
-$ch = curl_init ();
-// print_r($ch);
-curl_setopt ( $ch, CURLOPT_URL, $uri );
-curl_setopt ( $ch, CURLOPT_POST, 1 );
-curl_setopt ( $ch, CURLOPT_HEADER, 0 );
-curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
-$return = curl_exec ( $ch );
-curl_close ( $ch );
- 
-print_r($return);
+	$uri = "http://nycode.sinaapp.com/getpost.php";
+	// 参数数组
+	$data = array (
+	        'name' => 'nycode', 
+		'site' => 'http://www.nycode.cn'
+	);
+	var_dump($data);
+	$ch = curl_init ();
+	// print_r($ch);
+	curl_setopt ( $ch, CURLOPT_URL, $uri );
+	curl_setopt ( $ch, CURLOPT_POST, 1 );
+	curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+	$return = curl_exec ( $ch );
+	curl_close ( $ch );
+
+	print_r($return);
